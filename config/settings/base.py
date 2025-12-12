eab37a6ea0f512251fd457afb486e9b379623c9d
sainudheenp <sainudheen2003@gmail.com>
2025-12-12 16:34:33 +0530

    feat: Update CORS settings to include production domain and improve formatting

--- config/settings/base.py
+++ config/settings/base.py
@@ -193,8 +193,9 @@ SALES_IMPORT_API_KEY = os.getenv('SALES_IMPORT_API_KEY', 'WEDFBNPOIUFSDFTY')
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = os.getenv(
     'CORS_ALLOWED_ORIGINS',
-    'http://localhost:3000,http://localhost:3001,http://127.0.0.1:3000',
-    'https://alfa.imcbs.com/'
+    'http://localhost:3000,http://localhost:3001,' \
+    'http://127.0.0.1:3000',
+    'http://alfa.imcbs.com'
 ).split(',')
 
 CORS_ALLOW_CREDENTIALS = True
